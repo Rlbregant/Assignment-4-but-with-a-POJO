c6235f8b181ca4d1b645864d73d721d5f4854942
Ross <23426097+Rlbregant@users.noreply.github.com>
2022-12-20 09:50:28 -0700

    Final

--- src/com/coderscampus/WriteCSVFile.java
+++ src/com/coderscampus/WriteCSVFile.java
@@ -9,15 +9,16 @@ public class WriteCSVFile {
 	public void writeCSVFile(List<Student> students, String filename) {
 		try (PrintWriter pw = new PrintWriter(new File(filename))) {
 			// Print the column names
-			pw.println("Student ID    Student Name  	      Course  	Grade");
+			pw.println("Student ID,    Student Name,  	      Course,  	Grade");
 
 			// Print a line of dashes to separate the column names from the data
-			pw.println("------------  ------------  	      ------  	-----");
+			pw.println("------------,  ------------,  	      ------,  	-----");
 
 			// Iterate over the elements of the students list
 			for (Student student : students) {
-				// Print the data from the student object with left alignment
-				pw.printf("%-12s  %-20s  %-12s  %-12s%n", student.getId(), student.getName(), student.getCourse(), student.getGrade());
+				// Print the data from the student object
+				pw.println(student.getId() + ", " + student.getName() + ", " + student.getCourse() 
+				+ ", " + student.getGrade());
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
